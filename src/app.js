408f5cbac7b283d2b42d8429c009383024735743
kimb0078 <kimberly.suaris@outlook.com>
2021-04-13 05:08:25 +0200

    Css and html

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 require('./database/mongoose')
-const app = express()
 const router = require('./routers/routes')
 const userLimiter = require('./middleware/rateLimiter')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
-
-const port = 3000 || process.env.PORT
 const myDirectory = process.env.ROOT_DIRECTORY
+
+const app = express()
 const server = http.createServer(app)
 const io = socketio(server);
 
@@ -32,11 +31,12 @@ io.on('connection', socket => {
     })
 
     //Listen for chat message
-    socket.on('chatMessage', (msg) => {
+    socket.on('chatMessage', msg => {
         io.emit(msg) //Broadcasts to all users
     })
 })
 
+const port = 3000 || process.env.PORT
 server.listen(port, () => {
     console.log('Server is running on port ' + port)
 })
